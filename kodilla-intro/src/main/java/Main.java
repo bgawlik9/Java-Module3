16b7e3a8f13f34012cde3d106bb8457352d78968
Jan Kowalski <gwr9@tlen.pl>
2023-02-22 13:53:44 +0100

    commit modul 0.8

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        String Hello = "hello from FirstClass";
+        System.out.println(Hello);
+
+        MainExtended mainExtended = new MainExtended();
+
+
+        System.out.println(mainExtended.catName);
+        System.out.println(mainExtended.luckyNumber);
+        System.out.println(mainExtended.isBurekPretty);
+        System.out.println(mainExtended.luckyLetter);
+        System.out.println(mainExtended.engineName);
+        System.out.println(mainExtended.engine);
+
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,13 @@
+public class MainExtended {
+
+    public int luckyNumber = 11;
+    public String catName = "Burek";
+    public boolean isBurekPretty = true;
+    public char luckyLetter = 'V';
+    public double engine = 2.0;
+    public String engineName = "TDI";
+
+
+
+
+}
