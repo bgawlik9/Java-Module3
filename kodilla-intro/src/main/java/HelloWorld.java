888c9473d9e049dab715a17038238716e59ab1a8
Jan Kowalski <gwr9@tlen.pl>
2023-02-22 13:07:06 +0100

    first commit

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
         System.out.println("Bartosz");
         System.out.println("32");
         System.out.println("G");
