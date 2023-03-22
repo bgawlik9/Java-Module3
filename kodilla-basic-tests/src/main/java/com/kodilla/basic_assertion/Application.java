17daabbb8c88ea639379a39d713e6a724c921136
Jan Kowalski <gwr9@tlen.pl>
2023-03-22 16:48:11 +0100

    commit calculator tests 2

--- kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -3,33 +3,34 @@ package com.kodilla.basic_assertion;
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        int a =5;
-        int b =8;
-        int sumResult = calculator.sum(a,b);
-        boolean correct = ResultChecker.assertEquals(13,sumResult);
+        double b = 8.0;
+        double a = 5.0;
+        double sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(13.0, sumResult);
         if (correct) {
             System.out.println("Metoda sum działa poprawanie dla liczb " + a + " i " + b);
         } else {
-            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b );
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
         }
 
-        int subtractResult = calculator.subtract(a,b);
-        boolean correctSubtract = ResultChecker.assertEquals(-3,subtractResult);
-        if (correctSubtract){
+        double subtractResult = calculator.subtract(a, b);
+        boolean correctSubtract = ResultChecker.assertEquals(-3, subtractResult);
+        if (correctSubtract) {
             System.out.println("Metoda subtract działa poprawanie dla liczb " + a + " i " + b);
         } else {
-            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b );
+            System.out.println("Metoda subtract nie działa poprawnie dla liczb " + a + " i " + b);
         }
 
-        int squareResult = calculator.square(a);
-        boolean correctSquare = ResultChecker.assertEquals(25,squareResult);
-        if (correctSquare){
-            System.out.println("Metoda square działa poprawanie dla liczby " + a );
+        double squareResult = calculator.square(a);
+        boolean correctSquare = ResultChecker.assertEquals(25, squareResult);
+        if (correctSquare) {
+            System.out.println("Metoda square działa poprawanie dla liczby " + a);
         } else {
-            System.out.println("Metoda subtract nie działa poprawnie dla liczby " + a );
-        }
+            System.out.println("Metoda subtract nie działa poprawnie dla liczby " + a);
         }
 
-        }
+    }
+}
+
 
 
