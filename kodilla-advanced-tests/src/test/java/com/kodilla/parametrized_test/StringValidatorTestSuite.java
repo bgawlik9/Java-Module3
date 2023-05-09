cd6d04a409243efb595ce61e578a85a91fb20500
Jan Kowalski <gwr9@tlen.pl>
2023-05-09 14:25:13 +0200

    Tests

---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_test/StringValidatorTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.parametrized_test;
+
+import com.kodilla.parametrized_tests.StringValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringValidatorTestSuite {
+
+    private StringValidator validator = new StringValidator();
+
+    @Test
+    public void shouldReturnFalseIfStringIsNotEmpty() {
+        assertFalse(validator.isBlank("test"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnTrueIfStringIsEmpty(String text) {
+        System.out.println(" | " + text + " | ");
+        assertTrue(validator.isBlank(text));
+    }
+
+
+
+    @ParameterizedTest
+    @NullSource
+    public void shouldReturnTrueIfStringIsNull(String text) {
+        assertTrue(validator.isBlank(text));
+    }
+
+}
---
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_test/UserValidatorTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.parametrized_test;
+
+import com.kodilla.parametrized_tests.homework.UserValidator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class UserValidatorTestSuite {
+    UserValidator userValidator = new UserValidator();
+
+    @ParameterizedTest
+    @ValueSource(strings = {"test_name.32","joe70_","joe"})
+    public void shouldReturnTrueWhenUsernameIsCorrect(String username){
+        boolean result = userValidator.validateUsername(username);
+        assertTrue(result);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"@JOE", "test_name<3","Grzegorz_Brzeczeszczykiewicz", " "})
+    public void shouldReturnFalseWhenUserNameIsIncorrect(String username) {
+        boolean result = userValidator.validateUsername(username);
+        assertFalse(result);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"test@mail.com", "t.test-10@test-domain.pl"})
+    public void shouldReturnTrueWhenEmailAddressIsCorrect(String email) {
+        boolean result = userValidator.validateEmail(email);
+        assertTrue(result);
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"test'10@mail.com", "test@mail,com", "test@mail", "test.com", "test!@domain.pl"})
+    public void shouldReturnFalseWhenEmailAddressIsIncorrect(String email) {
+        boolean result = userValidator.validateEmail(email);
+        assertFalse(result);
+    }
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void shouldReturnWhenEmailAddressIsEmpty(String email) {
+        assertFalse(userValidator.validateEmail(email));
+    }
+
+
+}
