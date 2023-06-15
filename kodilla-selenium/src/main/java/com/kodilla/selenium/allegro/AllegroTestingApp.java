a5c38a36a30b7a3dc75f3ba642a76a885d80eaea
Jan Kowalski <gwr9@tlen.pl>
2023-06-15 14:06:21 +0200

    SELENIUM 3.0

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -13,12 +15,14 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://ebay.pl");
 
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='gdpr-banner-accept']")));
 
-        WebElement category = driver.findElement(By.xpath("//*[@id=\"gh-cat-box\"]/select"));
+        WebElement category = driver.findElement(By.xpath("//*[@id='gh-cat-box']/select"));
         Select categorySelect = new Select(category);
         categorySelect.selectByIndex(9);
 
-        WebElement searchField = driver.findElement(By.xpath("//*[@id=\"gh-ac-box2\"]/input"));
+        WebElement searchField = driver.findElement(By.xpath("//*[@id='gh-ac-box2']/input"));
         searchField.sendKeys("mavic mini");
 
         WebElement searchButton = driver.findElement(By.xpath("//input[@id='gh-btn']"));
